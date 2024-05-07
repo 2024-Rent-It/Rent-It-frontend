8e7a8394ef63a8ade976941b4efbd7a0eb47251c
juwon-heo <hjuwone2e@yonsei.ac.kr>
2024-05-07 19:57:48 +0900

    Update:Home 화면 하단 수정

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef, useState, useEffect } from 'react';
+import React, { useCallback, useRef, useState, useEffect, } from 'react';
 import { 
     FlatList, 
     ScrollView, 
@@ -6,7 +6,8 @@ import {
     Dimensions, 
     View, 
     StyleSheet, 
-    RefreshControl 
+    RefreshControl,
+    Text
 } from 'react-native';
 import styled from 'styled-components/native';
 import { useNavigation } from '@react-navigation/native';
@@ -224,7 +225,12 @@ const Home = () => {
             <PickerComponent />
             {renderIconItem()}
             {renderProductItem()}
-            <FooterRectangle />
+            <FooterRectangle>
+                <Text style={styles.footerText}>@RentIt Co.,Ltd. All Rights Reserved</Text>
+                <Text style={styles.footersmallText}>'렌팃' 앱에서 이루어지는 거래의 경우 '렌팃'은 통신판매의 당사자가 아닌 통신판매중개자로서 상품, 상품정보, 거래에 대한 책임이 제한될 수 있으므로, 각 상품 페이지에서 구체적인 내용을 확인하시기 바랍니다.</Text>
+                <Text style={styles.footermiddleText}>사업자 정보</Text>
+                <Text style={styles.footersmallText2}>(주)RentIt | 대표자 : 연세대 | 주소 : 연세대학교 미래캠퍼스</Text>
+            </FooterRectangle>
         </ScrollView>
     );
 };
@@ -247,7 +253,7 @@ const StyledProductContainer = styled.View`
 const FooterRectangle = styled.View`
     width: 100%;
     height: 150px;
-    background-color: #333;
+    background-color: #ECECEC;
     margin-top: 30px;
 `;
 
@@ -264,6 +270,38 @@ const styles = StyleSheet.create({
         borderRadius: 4,
         backgroundColor: '#CCCCCC',
         marginHorizontal: 5,
+    },
+    footerText: {
+        textAlign: 'center',
+        fontSize: 15,
+        fontWeight: 'regular',
+        color: 'black',
+        marginTop: 10,
+        marginRight: '36%',
+    },
+    footermiddleText: {
+        textAlign: 'center',
+        fontSize: 15,
+        fontWeight: 'regular',
+        color: 'black',
+        marginTop: 15,
+        marginRight: '78%',
+    },
+    footersmallText: {
+        fontSize: 13,
+        fontWeight: 'regular',
+        color: 'gray',
+        marginTop: 10,
+        marginRight: '3%',
+        marginLeft: '3%',
+    },
+    footersmallText2: {
+        fontSize: 13,
+        fontWeight: 'regular',
+        color: 'gray',
+        marginTop: 5,
+        marginRight: '3%',
+        marginLeft: '3%',
     },
 });
 
