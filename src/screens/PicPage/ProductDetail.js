d974b200de6f7c2d7c16b5190c97eda4101464a6
SW <sw@SWui-MacBookPro.local>
2024-05-07 19:35:52 +0900

    update:판매자 정보에서 productDetail로 넘어가기 수정

--- src/screens/PicPage/ProductDetail.js
+++ src/screens/PicPage/ProductDetail.js
@@ -8,6 +8,9 @@ import { useAuth } from '../../contexts/AuthContext';
 const ProductDetailPage = ({ route , navigation}) => {
   const { token, userNickname } = useAuth(); // 로그인된 사용자 토큰 가져오기
   const { id } = route.params;
+  // const { id } = navigation.id;
+  console.log("넘어갈 때 들어오는 Id값", id);
+
   const [isLiked, setIsLiked] = useState(false);
   const [categoryWidth, setCategoryWidth] = useState(0);
   const [product, setProduct] = useState('');
@@ -31,6 +34,7 @@ const ProductDetailPage = ({ route , navigation}) => {
     }
     getProductById();
     checkFavorite();
+    getProductById();
   }, [categoryWidth]);
 
   const checkFavorite = async () => {
